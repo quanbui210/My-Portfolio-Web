140de26a536d6d201aae8787626743bd9f07b148
Quan Bui <quanbui021001@gmail.com>
2022-11-14 19:15:51 +0200

    undo changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,14 +6,11 @@ import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt
 
 const Footer = () => {
     return (
-        <div className="contact row">
+        <div className="contact">
             <h1 className="contact-title">Contact Me</h1>
-            <div className='contact-detail'>At the moment, I am looking for an internship or <br></br>junior web development position in Finland. <br></br> Please contact me through: <br></br>
-            <LocalPhoneIcon/> : +358 465887841 (WhatsApp | SMS | Phone Call)<br></br>
+            <div className='contact-detail'>At the moment, I am looking for an internship or junior web development position in Finland. <br></br> Please contact me through: <br></br>
+            <LocalPhoneIcon/>: +358 465887841 (WhatsApp | SMS | Phone Call)<br></br>
             <EmailIcon/>: quanbui021001@gmail.com <br/> Or via my  <a href='https://www.linkedin.com/in/quan-bui-0a4908209/'>LinkedIn profile</a>
-            </div>
-            <div className="contact-end col">
-                <div>Looking foward to get in touch<br></br> and explore career options! <br></br>I appreciate your time and consideration <SentimentSatisfiedAltIcon/>.</div>
             </div>
         </div>
     )
